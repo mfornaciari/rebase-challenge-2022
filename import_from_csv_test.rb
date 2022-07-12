62a6448337e6b55a7d237d180cdb6433cb8d70cb
Marco Fornaciari <mfornaciari@gmail.com>
2022-07-12 19:08:01 -0300

    Cria método que popula BD a partir de arquivo CSV

---
+++ import_from_csv_test.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+require 'pg'
+require './import_from_csv'
+
+class TestImportFromCsv < Test::Unit::TestCase
+  def test_import_from_csv
+    data_from_csv = []
+    CSV.foreach('./test_data.csv', headers: true, col_sep: ';') { |row| data_from_csv << row.fields }
+    connection = PG.connect dbname: 'medical_records', host: 'database', user: 'user', password: 'password'
+
+    import_from_csv('./test_data.csv')
+    data_from_database = connection.exec('SELECT * FROM exams').map(&:values)
+    connection.close
+
+    assert_equal data_from_database, data_from_csv
+  end
+end
